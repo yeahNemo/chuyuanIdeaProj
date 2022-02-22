124494d77fcb5901c1ed5ae605368df775fc0a40
NEMO <974025642@qq.com>
2022-02-22 23:18:14 +0800

    引入mybatis和thymeleaf并测试成功

---
+++ src/main/java/com/nemo/servlet/StorageViewServlet.java
@@ -0,0 +1,24 @@
+package com.nemo.servlet;
+
+import com.nemo.myspringmvc.ViewBaseServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/html/admin/storage.html")
+public class StorageViewServlet extends ViewBaseServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("进入了StorageViewServlet");
+        super.processTemplate("html/index",req,resp);
+    }
+
+}
